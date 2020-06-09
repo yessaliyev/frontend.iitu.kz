1c36a96b572227be9a7fa377510fbe9bf50c6a1c
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2020-06-09 23:36:44 +0600

    fix titles

--- src/views/CoursesPage.vue
+++ src/views/CoursesPage.vue
@@ -3,7 +3,12 @@
         <NavBar/>
         <div class="main-block">
             <div class="left-block-nav">
-                <home-left-nav/>
+                <div class="first-nav">
+                    <home-left-nav/>
+                </div>
+                <div class="second-nav">
+                    <left-static-nav/>
+                </div>
             </div>
             <div class="main-block-items">
                 <div class="main-block-item">
@@ -19,10 +24,11 @@
     import UserCourses from "../components/courses/UserCourses";
     import HomeLeftNav from "../components/navigation/HomeLeftNav";
     import NavBar from "../components/navigation/NavBar";
+    import LeftStaticNav from "../components/navigation/LeftStaticNav";
 
     export default {
         name: "CoursesPage",
-        components: {UserCourses, HomeLeftNav, NavBar}
+        components: {UserCourses, HomeLeftNav, NavBar,LeftStaticNav}
     }
 </script>
 
